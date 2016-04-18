a492854be845c643c90c6a8907c04e3acc1dfc99
Stephen@rconfig3-5 <stephen@rconfig.com>
2016-04-18 18:09:37 +0100

    Updated ajaxPurgeSqlData.php for PDO/DB2

--- www/lib/ajaxHandlers/ajaxPurgeSqlData.php
+++ www/lib/ajaxHandlers/ajaxPurgeSqlData.php
@@ -1,53 +1,36 @@
 <?php
+// Purge Deleted Items from the Database. i.e. all items with a 'status' of 2
+// used here settings.js and settingsDb.js
 //php errors supressed on this pagre beacuse they should not interupt the JSON repsonse. 
 // i.e. if errors were made due to SQL errors etc.. JSOn would not be processed by JS on the SettingsDB.php page
 error_reporting(0); 
 session_start();
-require_once("../../../classes/db.class.php");
+require_once("../../../classes/db2.class.php");
 require_once("../../../classes/ADLog.class.php");
 require_once("../../../config/config.inc.php");
 
-$db  = new db();
+$db2  = new db2();
 $log = ADLog::getInstance();
 
-// query DB for all tables
+// query DB for all tables with 'status' columns
 if(defined('DB_NAME')){
-	$tblsSql = $db->q("SHOW TABLES FROM " . DB_NAME);
+    $db2->query("SELECT TABLE_NAME, COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS 
+                    WHERE TABLE_SCHEMA = '" . DB_NAME ."' 
+                    AND COLUMN_NAME = 'status'");
+    $tbls = $db2->resultsetCols();
 } else {
 	$response['response'] = "ERROR: executing query";
 	$log->Fatal("Fatal: " . mysql_error() . " (File: " . $_SERVER['PHP_SELF'] . ")");
 }
 
-// set and fill returnTbls Array
-$returnTbls = array();
-while ($row = mysql_fetch_assoc($tblsSql)) {
-    array_push($returnTbls, $row);
+foreach ($tbls as &$tbl) {
+    $db2->query("DELETE FROM " . $tbl . " WHERE status = 2");
+    if ($db2->execute()) {
+        $response['response'] = "SUCCESS: Deleted items purged";
+        $log->Info("Info: Deleted rows purged from Table:" . $tbl . "  (File: " . $_SERVER['PHP_SELF'] . ")");
+    } else {
+        $response['response'] = "ERROR: Could not delete rows. Errors logged to log file";
+        $log->Fatal("Fatal: " . mysql_error() . " (File: " . $_SERVER['PHP_SELF'] . ")");
+    }
 }
-
-// flatten array and get tbl names
-$response = array(); // set $$response array
-foreach ($returnTbls as $returnK) {
-    foreach ($returnK as $key => $tbl) {
-        // get list of columns for each table
-        $columnSql = $db->q("SHOW COLUMNS FROM " . $tbl);
-        
-        // loop over the columns SQL 
-        while ($row = mysql_fetch_assoc($columnSql)) {
-            // if status column is present, do next query
-            if ($row['Field'] == 'status') {
-                // get list of rows for each table where status = '2' deleted
-                if ($db->q("DELETE FROM " . $tbl . " WHERE status = 2")) {
-                    $response['response'] = "SUCCESS: Deleted items purged";
-                    $log->Info("Info: Deleted rows purged from Table:" . $tbl . "  (File: " . $_SERVER['PHP_SELF'] . ")");
-                } else {
-                    $response['response'] = "ERROR: Could not delete rows. Errors logged to log file";
-                    $log->Fatal("Fatal: " . mysql_error() . " (File: " . $_SERVER['PHP_SELF'] . ")");
-                }
-                
-            } // if
-        } // while
-    } // 2nd foreach
-} // 1st foreach
-
 echo json_encode($response);
-?>
