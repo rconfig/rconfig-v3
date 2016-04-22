e18fc8659e3011723dc38f351a5033fcf20a5744
Stephen@rconfig3-5 <stephen@rconfig.com>
2016-04-22 11:43:50 +0100

    commands* updated for PDO

--- www/commands.inc.php
+++ www/commands.inc.php
@@ -1,47 +1,48 @@
 <?php
 /* Includes */
-require_once("../classes/db.class.php");
+require_once("../classes/db2.class.php");
 include_once('../classes/paginator.class.php');
 
 /* Instantiate DB Class */
-$db = new db();
+$db2 = new db2();
 
-/* Get Row count from nodes where NOT deleted*/
-$rs              = $db->q('SELECT COUNT(*) AS total FROM configcommands WHERE status = 1');
-$row             = mysql_fetch_row($rs);
+/* Get Row count from configcommands where NOT deleted */
+$db2->query('SELECT COUNT(*) AS total FROM configcommands WHERE status = 1');
+$row = $db2->resultsetCols();
 $result["total"] = $row[0];
-
 /* Instantiate Paginator Class */
-$pages              = new Paginator;
+$pages = new Paginator;
 $pages->items_total = $result['total'];
-$pages->mid_range   = 7; // Number of pages to display. Must be odd and > 3
+$pages->mid_range = 7; // Number of pages to display. Must be odd and > 3
 $pages->paginate();
 ?>
 <!-- begin Search form -->
 <div id="deviceActionDiv">
-	<div id="searchForm"> 
-	<legend>Search</legend>
-		<form name ="searchForm" method="GET" action="commands.php" onsubmit="return searchValidateForm()">
-			<select name="searchColumn" id="searchColumn" class="paginate">
-				<option value="command">Command</option>
-			</select>
-			<select name="searchOption" id="searchOption" class="paginate">
-				<option value="contains" selected>Contains</option>
-				<option value="notContains">Not Contains</option>
-				<option value="equals">Equals</option>
-			</select>
-			<input type="text" id="searchField" name="searchField" placeholder="search text" class="paginate">
-			<input type="hidden" id="search" value="search" name="search">
-			<?php 
-			if(isset($errors['searchField'])){echo "<span class=\"error\">".$errors['searchField']."</span>";}
-			?>
-			<br/>
-			<button type="submit" class="paginate">Go!</button> <?php //search logic below in this script?>
-			<button onClick="clearSearch()" type="button" class="paginate">Clear Search</button>
-			<br />
-			<font size="0.3em">use '*' as a wildcard</font>
-		</form>
-	</div> <!-- end searchForm -->	
+    <div id="searchForm"> 
+        <legend>Search</legend>
+        <form name ="searchForm" method="GET" action="commands.php" onsubmit="return searchValidateForm()">
+            <select name="searchColumn" id="searchColumn" class="paginate">
+                <option value="command">Command</option>
+            </select>
+            <select name="searchOption" id="searchOption" class="paginate">
+                <option value="contains" selected>Contains</option>
+                <option value="notContains">Not Contains</option>
+                <option value="equals">Equals</option>
+            </select>
+            <input type="text" id="searchField" name="searchField" placeholder="search text" class="paginate">
+            <input type="hidden" id="search" value="search" name="search">
+            <?php
+            if (isset($errors['searchField'])) {
+                echo "<span class=\"error\">" . $errors['searchField'] . "</span>";
+            }
+            ?>
+            <br/>
+            <button type="submit" class="paginate">Go!</button> <?php //search logic below in this script ?>
+            <button onClick="clearSearch()" type="button" class="paginate">Clear Search</button>
+            <br />
+            <font size="0.3em">use '*' as a wildcard</font>
+        </form>
+    </div> <!-- end searchForm -->	
 </div>
 <div class="spacer"></div>
 <?php
@@ -50,55 +51,55 @@ echo "<span class=\"\">" . $pages->display_jump_menu() . $pages->display_items_p
 echo "<div class=\"spacer\" style=\"padding-bottom:3px;\"></div>";
 
 /* Search functionality
-1. check if hidden search input is set (i.e. submit for search for was pressed)
-2. check all inputs and return errors if needed - to be done
-3. if search fields are complete, build the query with the search string, esle default query inc pagnation for use later
-*/
+  1. check if hidden search input is set (i.e. submit for search for was pressed)
+  2. check all inputs and return errors if needed - to be done
+  3. if search fields are complete, build the query with the search string, esle default query inc pagnation for use later
+ */
 
-if (isset($_GET['search'])){
+if (isset($_GET['search'])) {
 
-	if (isset($_GET['searchColumn'])){
-		$searchColumn = $_GET['searchColumn'];
-	}
-	
-	if (isset($_GET['searchOption'])){
-		switch ($_GET['searchOption']) {
-			case "contains":
-			$searchOption = "LIKE";
-		break;
-			case "equals":
-			$searchOption = "=";
-		break;
-			case "notContains":
-			$searchOption = "NOT LIKE";
-		break;
-		}
-	}
-	
-	if (isset($_GET['searchField'])){
-		$searchField = mysql_real_escape_string($_GET['searchField']);
-		$searchField = str_replace("*", "%", $searchField); // swap * for % for SQL query
-		if ($searchOption == "LIKE" || $searchOption == "NOT LIKE"){
-			$searchField = '%'.$searchField.'%';
-		}
-	}					
-$q     = $db->q("SELECT id, command 
-					FROM configcommands 
-					WHERE status = 1
-					AND ".$searchColumn." ".$searchOption." '".$searchField."'
-					$pages->limit");
-	
+    if (isset($_GET['searchColumn'])) {
+        $searchColumn = $_GET['searchColumn'];
+    }
+
+    if (isset($_GET['searchOption'])) {
+        switch ($_GET['searchOption']) {
+            case "contains":
+                $searchOption = "LIKE";
+                break;
+            case "equals":
+                $searchOption = "=";
+                break;
+            case "notContains":
+                $searchOption = "NOT LIKE";
+                break;
+        }
+    }
+
+    if (isset($_GET['searchField'])) {
+        $searchField = mysql_real_escape_string($_GET['searchField']);
+        $searchField = str_replace("*", "%", $searchField); // swap * for % for SQL query
+        if ($searchOption == "LIKE" || $searchOption == "NOT LIKE") {
+            $searchField = '%' . $searchField . '%';
+        }
+    }
+    $db2->query("SELECT id, command FROM configcommands WHERE status = 1
+		AND " . $searchColumn . " " . $searchOption . " '" . $searchField . "'
+		$pages->limit");  
+    $db2->bind(':searchColumn', $searchColumn);
+    $db2->bind(':searchOption', $searchOption);
+    $db2->bind(':searchField', $searchField);
+    $qRes  = $db2->resultset();
+    
 } else {
-/* GET all nodes records from DB */
-$q     = $db->q("SELECT id, command 
-					FROM configcommands 
-					WHERE status = 1
-					$pages->limit");
-	}
-	
+    /* GET all nodes records from DB */
+    $db2->query("SELECT id, command FROM configcommands WHERE status = 1 $pages->limit");  
+    $qRes  = $db2->resultset();    
+}
+
 // push rows to $items array
 $items = array();
-while ($row = mysql_fetch_assoc($q)) {
+foreach ($qRes as $row) {
     array_push($items, $row);
 }
 
@@ -109,48 +110,49 @@ $i = 0; # row counter  to enable alternate row coloring
 ?>
 
 <table id="commandsTbl" class="tableSimple">
-	<thead>
-		<th><input type="checkbox" disabled="disabled"/></th>
-		<th align="left">Command</th>
-		<th align="left">Category</th>
-	</thead>
-	<tbody>
-	<?php
-	foreach ($result['rows'] as $rows):
-		$cmdId   = $rows['id'];
-		$command = $rows['command'];
-		$catName = array(); // set catName var as Array
-		$catQ    = $db->q('SELECT cct.nodeCatId, cat.categoryName
-						FROM cmdCatTbl AS cct 
-						LEFT JOIN categories AS cat ON cct.nodeCatId = cat.id
-						WHERE configCmdId = ' . $cmdId); 
-			
-	// get Query results and push to catName array then impode to string
-		while ($row = mysql_fetch_assoc($catQ)) {
-			// set the array and fill it
-			array_push($catName, $row['categoryName']);
-		}
-		//check if array is full or empty and set var
-		if (empty($catName)) {
-			$catList = "<em><font color='red'>No categories Selected</font></em>";
-		} else {
-			$catList = implode(", ", $catName);
-		}
-		
-		/* This bit just updates the class='row' bit with an alternating 1 OR 0 for alternative row coloring*/
-		echo '<tr class="row' . ($i++ % 2) . '">';
-		echo '<td align="center"><input type="checkbox" id="' . $cmdId . '"></td>';
-		echo '<td align="left">' . $command . '</td>';
-		echo '<td align="left">' . $catList . '</td>';
-		echo '</tr>';
-	endforeach;
-	?>
-	
-	</tbody>
+    <thead>
+    <th><input type="checkbox" disabled="disabled"/></th>
+    <th align="left">Command</th>
+    <th align="left">Category</th>
+</thead>
+<tbody>
+    <?php
+    foreach ($result['rows'] as $rows):
+        $cmdId = $rows['id'];
+        $command = $rows['command'];
+        $catName = array(); // set catName var as Array
+        $db2->query('SELECT cct.nodeCatId, cat.categoryName
+                        FROM cmdCatTbl AS cct 
+                        LEFT JOIN categories AS cat ON cct.nodeCatId = cat.id
+                        WHERE configCmdId = :cmdId');  
+        $db2->bind(':cmdId', $cmdId);
+        $catQ  = $db2->resultset();
+
+        // get Query results and push to catName array then impode to string
+        foreach ($catQ as $row) {
+            // set the array and fill it
+            array_push($catName, $row['categoryName']);
+        }
+        //check if array is full or empty and set var
+        if (empty($catName)) {
+            $catList = "<em><font color='red'>No categories Selected</font></em>";
+        } else {
+            $catList = implode(", ", $catName);
+        }
+
+        /* This bit just updates the class='row' bit with an alternating 1 OR 0 for alternative row coloring */
+        echo '<tr class="row' . ($i++ % 2) . '">';
+        echo '<td align="center"><input type="checkbox" id="' . $cmdId . '"></td>';
+        echo '<td align="left">' . $command . '</td>';
+        echo '<td align="left">' . $catList . '</td>';
+        echo '</tr>';
+    endforeach;
+    ?>
+
+</tbody>
 </table>
 
-<?php 
-echo $pages->display_pages(); 
+<?php
+echo $pages->display_pages();
 echo "<div class=\"spacer\"></div>";
 echo "<p class=\"paginate\">Page: $pages->current_page of $pages->num_pages</p>\n";
-?>
--- www/lib/crud/commands.crud.php
+++ www/lib/crud/commands.crud.php
@@ -1,10 +1,10 @@
 <?php
-require_once("../../../classes/db.class.php");
+require_once("../../../classes/db2.class.php");
 require_once("../../../classes/ADLog.class.php");
 require_once("../../../config/config.inc.php");
 require_once("../../../config/functions.inc.php");
 
-$db  = new db();
+$db2 = new db2();
 $log = ADLog::getInstance();
 
 /* Add Categories Here */
@@ -13,11 +13,10 @@ if (isset($_POST['add'])) {
     session_start();
     $errors = array();
     
-    
     /* validations */
-    
+
     // validate command field
-    
+
     if (!empty($_POST['command'])) {
         $command = str_replace(" ", "", $_POST['command']); // remove spaces for alpha validation
         if (ctype_alpha($command)) {
@@ -25,56 +24,54 @@ if (isset($_POST['add'])) {
         } else {
             $errors['command'] = "Command input invalid";
         }
-        
     } else {
         $errors['command'] = "Command field cannot be empty";
     }
-    
-    
+
+
     if (preg_match('/(?:\\\[trn])+/', $_POST['command'])) {
         $errors['command'] = "Command input invalid";
     } else {
-        $command = mysql_real_escape_string($_POST['command']); // reset $command var to actual input and escape
+        $command = $_POST['command']; // reset $command var to actual input and escape
     }
-    
-    
+
+
     /* if command inputted and no cat selected - return error */
     $catId = $_POST['catId'];
-    
+
     if ($catId['0'] == 0) {
         $errors['catId'] = "You must select a category";
     } else {
         $catId = $_POST['catId'];
     }
-    
-    
+
+
     if (!empty($command) && $catId['0'] != 0) {
         /* Begin DB query. This will either be an Insert if $_POST editid is not set - or an edit/Update if editid is set in POST */
-        
+
         if (empty($_POST['editid'])) { // actual add
-            
             // add command to configcommand table
-            $q = "INSERT INTO configcommands
-							(command, 
-							status) 
-							VALUES 
-								('" . $command . "', 
-								'1'
-								)";
-            
-            if ($result = $db->q($q)) {
-                $cmdIdQ = $db->q('SELECT id FROM configcommands WHERE command = "' . $command . '"');
-                while ($row = mysql_fetch_assoc($cmdIdQ)) {
+            $db2->query("INSERT INTO configcommands (command, status) VALUES (:command, '1' )");
+            $db2->bind(':command', $command); 
+            $queryResult = $db2->execute();
+            if($queryResult){
+                $db2->query("SELECT id FROM configcommands WHERE command = :command");
+                $db2->bind(':command', $command); 
+                $commandRes = $db2->resultset();                
+                foreach ($commandRes as $row) {
                     $cmdId = $row['id'];
                 }
-                
+
                 // next loop over catId Post and get all IDs selected and insert to cmdCatTbl
                 $catIds = $_POST['catId'];
                 for ($i = 0; $i < count($catIds); $i++) {
                     $catId = $catIds[$i];
-                    $db->q('INSERT INTO cmdCatTbl (configCmdId, nodeCatId) VALUES (' . $cmdId . ', ' . $catId . ')');
+                    $db2->query('INSERT INTO cmdCatTbl (configCmdId, nodeCatId) VALUES (:cmdId, :catId)');
+                    $db2->bind(':cmdId', $cmdId); 
+                    $db2->bind(':catId', $catId); 
+                    $db2->execute();  
                 }
-                
+
                 $errors['Success'] = "Added command '" . $command . "' to Database";
                 $log->Info("Success: Added command " . $command . " to DB (File: " . $_SERVER['PHP_SELF'] . ")");
                 $_SESSION['errors'] = $errors;
@@ -89,9 +86,8 @@ if (isset($_POST['add'])) {
                 header("Location: " . $config_basedir . "commands.php?error");
                 exit();
             }
-            
         } else { // actual edit
-            
+
             /* validate editid is numeric */
             if (ctype_digit($_POST['editid'])) {
                 $id = $_POST['editid'];
@@ -103,21 +99,26 @@ if (isset($_POST['add'])) {
                 header("Location: " . $config_basedir . "categories.php?error");
                 exit();
             }
-            
-            $q = "UPDATE configcommands SET command = '" . $command . "'
-						WHERE id = " . $id;
-            
-            if ($result = $db->q($q)) {
+
+            $db2->query("UPDATE configcommands SET command = :command WHERE id = :id");
+            $db2->bind(':command', $command); 
+            $db2->bind(':id', $id); 
+            $queryResult = $db2->execute();
+            if ($queryResult) {
                 //then delete all entires from the cmdCatTbl with related ID first. Then update with new values
-                $db->q("DELETE FROM cmdCatTbl WHERE configCmdId = " . $id);
-                
+                $db2->query("DELETE FROM cmdCatTbl WHERE configCmdId = :id");
+                $db2->bind(':id', $id); 
+                $db2->execute();
                 // next loop over catId Post and get all IDs selected and insert to cmdCatTbl
                 $catIds = $_POST['catId'];
                 for ($i = 0; $i < count($catIds); $i++) {
                     $catId = $catIds[$i];
-                    $db->q('INSERT INTO cmdCatTbl (configCmdId, nodeCatId) VALUES (' . $id . ', ' . $catId . ');');
-                }
-                
+                    $db2->query('INSERT INTO cmdCatTbl (configCmdId, nodeCatId) VALUES (:id, :catId);');
+                    $db2->bind(':catId', $catId); 
+                    $db2->bind(':id', $id); 
+                    $db2->execute();
+            }
+
                 // return success
                 $errors['Success'] = "Edited command '" . $command . "' in Database";
                 $log->Info("Success: Edited command " . $command . " to DB (File: " . $_SERVER['PHP_SELF'] . ")");
@@ -134,9 +135,8 @@ if (isset($_POST['add'])) {
                 exit();
             }
         }
-        
     }
-    /* end 'id' post check*/
+    /* end 'id' post check */
 }
 
 // set the session id if any errors occur and redirect back to devices page with ?error set for JS on that page to keep form open 
@@ -148,17 +148,19 @@ if (!empty($errors)) {
     exit();
 }
 
-/* end 'add' if*/
+/* end 'add' if */
 
 
-/* begin delete check */
-elseif (isset($_POST['del'])) {
-    /* the query*/
-    $q = "UPDATE configcommands SET status = 2 WHERE id = " . $_POST['id'] . ";";
-    
-    if ($result = $db->q($q)) {
+/* begin delete check */ elseif (isset($_POST['del'])) {
+    /* the query */
+    $db2->query("UPDATE configcommands SET status = 2 WHERE id = " . $_POST['id'] . ";");
+    $db2->bind(':id', $_POST['id']); 
+    $queryResult = $db2->execute();
+    if ($queryResult) {
         //then delete all entires from the cmdCatTbl with related ID first. Then update with new values
-        $db->q("DELETE FROM cmdCatTbl WHERE configCmdId = " . $_POST['id']);
+        $db2->query("DELETE FROM cmdCatTbl WHERE configCmdId = :id");
+        $db2->bind(':id', $_POST['id']); 
+        $db2->execute();
         $log->Info("Success: Deleted Command " . $_POST['id'] . " in DB (File: " . $_SERVER['PHP_SELF'] . ")");
         $response = json_encode(array(
             'success' => true
@@ -169,12 +171,10 @@ elseif (isset($_POST['del'])) {
             'failure' => true
         ));
     }
-    
-    echo $response;
-} /* end 'delete' if*/ /* Below is used for an ajax call from vendors update 
-jquery function to get row information to present back to vendor edit form*/ 
 
-elseif (isset($_GET['getRow']) && isset($_GET['id'])) {
+    echo $response;
+} /* end 'delete' if */ /* Below is used for an ajax call from vendors update 
+  jquery function to get row information to present back to vendor edit form */ elseif (isset($_GET['getRow']) && isset($_GET['id'])) {
     if (ctype_digit($_GET['id'])) {
         $id = $_GET['id'];
     } else {
@@ -185,21 +185,15 @@ elseif (isset($_GET['getRow']) && isset($_GET['id'])) {
         header("Location: " . $config_basedir . "categories.php?error");
         exit();
     }
-    
-    $q     = $db->q("SELECT 
-			command
-		FROM configcommands
-		WHERE status = 1
-		AND id =" . $id);
+    $db2->query("SELECT command FROM configcommands WHERE status = 1 AND id = :id");
+    $db2->bind(':id', $id); 
+    $queryResult = $db2->resultset();    
     $items = array();
-    while ($row = mysql_fetch_assoc($q)) {
+    foreach ($queryResult as $row) {
         array_push($items, $row);
     }
-    
+
     $result["rows"] = $items;
     echo json_encode($result);
-    
 }
 /* end GetId */
-
-?>
