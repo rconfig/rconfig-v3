36a00ea48c069dca7eaed2096cbdf379d5d4e4d4
Stephen Stack <stephen@rconfig.com>
2020-07-25 23:16:24 +0000

    added empty tmp dir to git

--- www/install/lib/ajaxHandlers/ajaxGetHttpdVer.php
+++ www/install/lib/ajaxHandlers/ajaxGetHttpdVer.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 // check HTTP version and return to install pages
 function find_HTTPD_Version()
 {
