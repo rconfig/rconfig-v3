e17768347371347c7a86e49d9fd244ece3210199
root <root@dev1.nmstj53ng4tejo4pzjldyxamfh.fx.internal.cloudapp.net>
2020-07-25 22:53:55 +0000

    fixed apache check if missing apache_get_version function

--- www/install/lib/ajaxHandlers/ajaxGetHttpdVer.php
+++ www/install/lib/ajaxHandlers/ajaxGetHttpdVer.php
@@ -1,10 +1,24 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // check HTTP version and return to install pages
-function find_HTTPD_Version() {
-    $output = apache_get_version();
-    preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version);
-    return $version[0];
+function find_HTTPD_Version()
+{
+    if (!function_exists('apache_get_version')) {
+        if (!isset($_SERVER['SERVER_SOFTWARE']) || strlen($_SERVER['SERVER_SOFTWARE']) == 0) {
+            return false;
+        }
+        return (float) filter_var($_SERVER["SERVER_SOFTWARE"], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+    } else {
+        $output = apache_get_version();
+        preg_match('@[0-9]+\.[0-9]+\.[0-9]+@', $output, $version);
+        return $version[0];
+    }
+
 }
+
 if (find_HTTPD_Version() >= 2.2) {
     $response = '<strong><font class="good">Yes</font></strong>';
 } else {
